a4f36f84aa74bbe1da24fe46c120332205808a4e
Dawoon <87120274+JDW611@users.noreply.github.com>
2025-05-10 07:46:21 +0900

    Time: 1 ms (78.22%), Space: 64.4 MB (71.36%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    if (k > nums.length) {
+        k %= nums.length;
+    }
+
+    const spliceArr = nums.splice(0, nums.length - k);
+
+    spliceArr.forEach((num) => nums.push(num));
+};
