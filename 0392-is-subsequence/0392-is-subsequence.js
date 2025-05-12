1eb634ded6148e2e73ca1bc7ace13bbfb61786be
Dawoon <87120274+JDW611@users.noreply.github.com>
2025-05-12 16:45:37 +0900

    Time: 2 ms (35.6%), Space: 54.5 MB (80.41%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+    if (s.length > t.length) return false;
+
+    let i = 0;
+    for(j=0; j<t.length; j++) {
+        if(s[i] === t[j]) i++;
+    }
+
+    return i === s.length;
+};
