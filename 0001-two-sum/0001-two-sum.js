da4d38de4ecdce8acb72a9f5dd63ece36f0b9b5b
Dawoon <87120274+JDW611@users.noreply.github.com>
2025-05-14 20:18:00 +0900

    Time: 1 ms (87.28%), Space: 55.5 MB (31.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,32 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    for (i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        const index = nums.lastIndexOf(complement);
+    const map = new Map();
 
-        if (index !== -1 && index !== i) {
-            return [i, index]
-        }
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if(map.has(complement)) {
+            const index = map.get(complement);
+
+            return [index, i];
+        } 
+        map.set(nums[i], i)
     }
 
-    return index;
+    return [];
+    
 };
+
+
+// 너무 느림
+// for (i = 0; i < nums.length; i++) {
+//         const complement = target - nums[i];
+//         const index = nums.lastIndexOf(complement);
+
+//         if (index !== -1 && index !== i) {
+//             return [i, index]
+//         }
+//     }
+
+//     return index;
