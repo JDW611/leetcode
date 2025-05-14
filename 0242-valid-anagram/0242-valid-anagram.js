e159baadd60995db9cacc6b8a00592834d23fe2d
Dawoon <87120274+JDW611@users.noreply.github.com>
2025-05-14 19:40:38 +0900

    Time: 217 ms (5.02%), Space: 61.3 MB (7.77%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) {
+        return false;
+    }
+
+    for(i=0; i<s.length; i++) {
+        t = t.replace(s[i],'');
+    }
+
+    return t === '';
+};
