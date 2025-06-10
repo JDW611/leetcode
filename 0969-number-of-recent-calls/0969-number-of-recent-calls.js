3eff8aa75c26014d16189d6dba18b5fe7cfb4248
Dawoon <87120274+JDW611@users.noreply.github.com>
2025-06-10 20:27:18 +0900

    Time: 29 ms (90.57%), Space: 69.1 MB (78.07%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.js
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.js
@@ -10,17 +10,11 @@ var RecentCounter = function() {
 RecentCounter.prototype.ping = function(t) {
     this.requests.push(t);
     
-    let left = 0, right = this.requests.length - 1;
-    while (left < right) {
-        let mid = Math.floor((left + right) / 2);
-        if (this.requests[mid] >= t - 3000) {
-            right = mid;
-        } else {
-            left = mid + 1;
-        }
+    while (this.requests.length > 0 && this.requests[0] < t - 3000) {
+        this.requests.shift();
     }
-    
-    return this.requests.length - left;
+
+    return this.requests.length;
 };
 
 /** 
